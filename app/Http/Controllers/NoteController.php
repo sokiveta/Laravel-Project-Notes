d23d4eebb15748d6a88ebc1f19edfae00da79db0
Michael Sokiveta <sokiveta@gmail.com>
2024-06-30 02:23:58 -0700

    Added user auth

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -12,7 +12,10 @@ class NoteController extends Controller
      */
     public function index()
     {
-        $notes = Note::query()->orderBy('created_at', 'desc')->paginate();
+        $notes = Note::query()
+        ->where('user_id', request()->user()->id)
+        ->orderBy('created_at', 'desc')
+        ->paginate();
         // dd($notes);
         return view('note.index',['notes' => $notes]); 
     }
@@ -30,7 +33,15 @@ class NoteController extends Controller
      */
     public function store(Request $request)
     {
-        return 'store';
+        $data = $request->validate([
+            'note' => ['required', 'string']
+        ]);
+
+        $data['user_id'] = $request->user()->id; 
+        $note = Note::create($data);
+
+        return to_route('note.show', $note)->with('message', 'Note was created');
+
     }
 
     /**
@@ -38,6 +49,9 @@ class NoteController extends Controller
      */
     public function show(Note $note)
     {
+        if ($note->user_id !== request()->user()->id) {
+            abort(403);
+        }
         return view('note.show',['note' => $note]);
     }
 
@@ -54,7 +68,14 @@ class NoteController extends Controller
      */
     public function update(Request $request, Note $note)
     {
-        return 'update';
+        $data = $request->validate([
+            'note' => ['required', 'string']
+        ]);
+
+        // $data['user_id'] = 1; // $request->user()->id
+        $note->update($data);
+
+        return to_route('note.show', $note)->with('message', 'Note was updated');
     }
 
     /**
@@ -62,6 +83,7 @@ class NoteController extends Controller
      */
     public function destroy(Note $note)
     {
-        return 'destroy';
+        $note->delete();
+        return to_route('note.index')->with('message', 'Note was deleted');
     }
 }
